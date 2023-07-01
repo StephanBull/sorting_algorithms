d67427cbbc39a96ebafa160d9087f5d5aa584a25
StephanBull <ndegwastephan@gmail.com>
2023-07-01 08:20:45 +0000

    Updadting buble sort file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,45 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * swap_integers - swaps two integersin an array
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_integers(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Description:prints an  array each time you swap two elements
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, len = size;
+	bool bubbly = false;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[i] > array[i + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				swap_integers(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
+		len--;
+	}
 }
